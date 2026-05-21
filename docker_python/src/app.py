38591df647178841635c3e7f93cc480878a56d5e
docker-cb-user <docker-cb-user@exsmple.com>
2026-05-22 00:14:16 +0530

    UI frontend changes

--- docker_python/src/app.py
+++ docker_python/src/app.py
@@ -3,11 +3,11 @@ import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
 
-st.set_page_config(page_title="Financial Planning Calculator")
+st.set_page_config(page_title="Scoopen Financial Planning Calculator")
 
-st.title("Financial Planning Calculator-December-2022")
+st.title("Scoopen Financial Planning Calculator-May-2026")
 
-st.header("**Monthly Income-September-2022**")
+st.header("**Monthly Income-May-2026**")
 st.subheader("Salary-2022")
 colAnnualSal, colTax = st.columns(2)
 
