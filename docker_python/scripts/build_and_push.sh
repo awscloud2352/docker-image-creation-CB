609dad9eeac8857f72a1295cf24c515094098b32
awscloud2352 <poonamapotikar7@gmail.com>
2026-05-22 00:33:15 +0530

    Update build_and_push.sh

--- docker_python/scripts/build_and_push.sh
+++ docker_python/scripts/build_and_push.sh
@@ -2,7 +2,7 @@
 # set -e
 # This script shows how to build the Docker image and push it to ECR to be ready for use
 # by SageMaker.
-
+#
 # The argument to this script is the image name. This will be used as the image on the local
 #machine and combined with the account and region to form the repository name for ECR.
 #Algorithm Name will be the Reposistory Name that is passed as a command line parameter.
